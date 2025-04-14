d8b4909ab6656ff1c670b6f01a42dd8b74895444
Safaholidays <safaholidays0@gmail.com>
2025-04-15 00:07:57 +0200

    Update route.ts

--- app/api/send-medical/route.ts
+++ app/api/send-medical/route.ts
@@ -1,9 +1,10 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
   const body = await req.json();
+
+  // استدعاء المفتاح هنا داخل الفنكشن فقط
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
   try {
     const data = await resend.emails.send({
