321e3188dd53abd862178d69747a4b87dd08080a
Safaholidays <safaholidays0@gmail.com>
2025-04-15 00:50:30 +0200

    Update route.ts

--- app/api/study-abroad/route.ts
+++ app/api/study-abroad/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
     const base64 = buffer.toString('base64');
 
     const response = await resend.emails.send({
-      from: 'info@safaholidays.com',
+      from: 'medical@resend.dev',
       to: 'safaholidays0@gmail.com',
       subject: `طلب جديد للسياحة العلاجية - ${patientName}`,
       html: `
