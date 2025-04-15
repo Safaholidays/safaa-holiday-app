763def4eb3584cab882dd41a1f0bd8d5d81bc7a0
Safaholidays <safaholidays0@gmail.com>
2025-04-15 22:10:15 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        <meta name="google-site-verification" content="googled7f9e7070bfbb89b.html" />
+      </head>
       <body className="font-sans bg-gray-50 text-gray-800">
         <Navbar />
         {children}
