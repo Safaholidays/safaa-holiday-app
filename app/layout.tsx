ad3c5e370f01f84bede696c3bb37f893c1bd198f
Safaholidays <safaholidays0@gmail.com>
2025-04-16 00:38:51 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ar" dir="rtl">
       <head>
-        <meta name="google-site-verification" content="googled7f9e7070bfbb89b.html" />
+        <meta name="google-site-verification" content="42kU1SCC-NPYJfTv_kbhf7FcQH5Wb8Jg6lPkC4tCSc4" />
       </head>
       <body className="font-sans bg-gray-50 text-gray-800">
         <Navbar />
